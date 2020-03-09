0546de00aa5c065e03c519dff8b3d8f2a713613d
root <root@ip-172-31-24-140.ec2.internal>
2020-03-09 21:23:08 +0800

    update aggregator

--- model/aggregator.go
+++ model/aggregator.go
@@ -1,30 +1,33 @@
 package model
 
 import (
+    "time"
+    "github.com/prometheus/common/model"
     "github.com/prometheus/prometheus/prompb"
     "github.com/hashicorp/terraform/helper/hashcode"
 )
 
 type block struct {
+    timestamp time.Duration
     data map[string]*prompb.TimeSeries
 }
 
 type cache struct {
-    data map[int]string
+    data map[int]float64
 }
 
 type Aggregator struct {
     whiteList []string
     jobName string
     prevCache *cache
-    aggCache *cache
-    packing *block
-    writing *block
+    sumCache *cache
+    pack *block
+    ready *block
 }
 
 type Aggregators struct {
     jobNum int
-    aggs  map[string]*Aggregator
+    aggregators map[string]*Aggregator
 }
 
 var Collection Aggregators
@@ -37,26 +40,48 @@ func NewAggregator(jobName string) *Aggregator {
     return &Aggregator{
         whiteList: Conf.whitelist,
         jobName:   jobName,
-        prevCache: &cache{data: make(map[int]string)},
-        aggCache:  &cache{data: make(map[int]string)},
-        packing:   &block{data: make(map[string]*prompb.TimeSeries)},
-        writing:   &block{data: make(map[string]*prompb.TimeSeries)},
+        prevCache: &cache{data: make(map[int]*prompb.Sample)},
+        sumCache:  &cache{data: make(map[int]*prompb.Sample)},
+        pack:   &block{data: make(map[string]*prompb.TimeSeries)},
+        ready:   &block{data: make(map[string]*prompb.TimeSeries)},
     }
 }
 
 func NewAggregators(jobNames []string) *Aggregators {
-    aggregators := &Aggregators{
+    aggs := &Aggregators{
         jobNum: len(jobNames),
-        aggs: make(map[string]*Aggregator, len(jobNames)),
+        aggregators: make(map[string]*Aggregator, len(jobNames)),
     }
     for _, jobName := range jobNames {
-        aggregators.aggs[jobName] = NewAggregator(jobName)
+        aggs.aggregators[jobName] = NewAggregator(jobName)
     }
-    return aggregators
+    return aggs
 }
 
-func (collection *Aggregators) UpdateOne(ts *prompb.TimeSeries) {
-     
+func (collection *Aggregators) updatePrevCache(hc int, prompb.Sample) float64 {
+    //
+}
 
+func (collection *Aggregators) updateSumCache(hc int, prompb.Sample, incVal float64) int {
+    //
+}
 
+func (collection *Aggregators) pack(sumVal float64) {
+    //
+}
+
+func (collection *Aggregators) send() {
+    //
+}
+
+func (collection *Aggregators) MergeMetric(ts *prompb.TimeSeries) {
+    m := make(model.Metric, len(ts.Labels))
+    for _, l := range *ts.Labels {
+        m[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+    }
+    metrics := fmt.Sprintf(m)
+    hc := hashcode(metrics)
+    incVal := collection.updatePrevCache(hc, ts.Sample)
+    sumVal := collection.updateSumCache(hc, ts.Sample, incVal)
+    colleciton.pack(sumVal)
 }
